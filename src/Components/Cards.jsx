72ba583ae27258dd5a135504d622ebe883d0a7fa
Nida Batool <nida@example.com>
2024-11-30 11:53:10 +0500

    Initial commit

---
+++ src/Components/Cards.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../Style/Cards.scss'
+
+const Cards = () => {
+  return (
+    <h1>Cardsgsjhdhs</h1>
+  )
+}
+
+export default Cards
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Components/Header'
+import Cards from '../Components/Cards'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Cards/>
+    </>
+  )
+}
+
+export default Home
