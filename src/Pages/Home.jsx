a385b5bdcc8532f5b2235ac06cfde89e8f701d81
Nida Batool <nida@example.com>
2024-11-30 12:01:38 +0500

    create a new file

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Cards from '../Components/Cards'
+import About from '../Components/About'
 
 const Home = () => {
   return (
     <>
     <Header/>
     <Cards/>
+    <About/>
     </>
   )
 }
