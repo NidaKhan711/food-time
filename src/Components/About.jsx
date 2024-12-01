f53d1b0b140ff10938829833864216366a7f0e09
Nida Batool <nida@example.com>
2024-12-01 19:37:11 +0500

    Add JSON setup

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Style/About.scss'
 
 const About = () => {
   return (
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,6 +4,13 @@ import Cards from '../Components/Cards'
 import About from '../Components/About'
 
 const Home = () => {
+  let headerData =[
+    {text1:'Delicious ',text2 :'Pizza',text3:' with perfect toppings and crust'},
+    {text1:'Juicy',text2:'Burger',text3:'with premium ingredients and sauces'},
+    {text1:'Hearty',text2:'Sandwich',text3:'with fresh, quality ingredients in every bite.'},
+  ]
+  const [headerCount,setHeaderCount] = useState(0);
+
   return (
     <>
     <Header/>
